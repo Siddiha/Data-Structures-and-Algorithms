e518786669e440657a1c8375cf9583b13ed19eff
Siddiha <fathimasiddika62@gmail.com>
2026-02-20 14:55:10 +0530

    added the insertion and deletion

--- linked List/Node.java
+++ linked List/Node.java
@@ -8,6 +8,51 @@ public class Node {
 
     }
 
+    static Node insertAtfront(Node head, int x){
+        Node newnode = new Node(x);
+        newnode.next = head;
+        return newnode;
+    }
+
+    static Node insertAtlast(Node head, int x){
+        Node newNode = new Node(x);
+        Node temp = head;
+
+        while (temp.next != null){
+            temp =temp.next;
+        }
+        temp.next = new Node(x);
+        return head;
+
+    }
+
+    static Node deleteNode(Node head){
+        if (head == null){
+            return null;
+        }
+
+        Node temp = head;
+        head = head.next;
+        return head;
+    }
+
+    static Node deletedtail(Node head){
+        if (head == null){
+            return null;
+        }
+        if (head.next == null){
+            return null;
+        }
+
+        Node temp = head;
+        while (temp.next.next == null){
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        return head;
+
+    }
     class Main{
         public static void main(String[] args) {
             Node head = new Node(10);
@@ -20,7 +65,7 @@ public class Node {
             while (temp != null){
                 System.out.println(temp.data + " ");
                 temp = temp.next;
-                
+
             }
         }
     }
